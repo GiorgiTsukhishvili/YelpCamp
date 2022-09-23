2e322cfbc59abafb1af473231d57c978bf1f6571
Giorgi Tsukhishvili <gcuxishvili@gmail.com>
2022-09-23 22:47:09 +0400

    Styling all the camps on main page

--- client/src/components/campGrounds/CampGrounds.tsx
+++ client/src/components/campGrounds/CampGrounds.tsx
@@ -18,13 +18,23 @@ const CampGrounds = () => {
   }, []);
   console.log(campGrounds);
   return (
-    <div>
+    <div className="mx-auto my-10 flex flex-col gap-10">
       {campGrounds &&
         campGrounds.map((item) => (
-          <Link to={`/campgrounds/${item._id}`} key={v4()}>
-            <h1>{item.title}</h1>
-            <h1>{item.location}</h1>
-          </Link>
+          <div key={v4()} className="border-gray-400 border-[1.5px] flex">
+            <img src={item.image} alt="" className="w-[350px] h-[500px]" />
+            <div className="w-[700px] py-2 px-5">
+              <h1 className="text-2xl font-bold pb-5">{item.title}</h1>
+              <h1>{item.description}</h1>
+              <h1 className=" pt-5 mb-10  text-gray-500">{item.location}</h1>
+              <Link
+                to={`/campgrounds/${item._id}`}
+                className="bg-blue-500 text-white px-5 py-2 text-md rounded-[8px] hover:bg-blue-600 transition-all duration-300"
+              >
+                View Camp
+              </Link>
+            </div>
+          </div>
         ))}
     </div>
   );
